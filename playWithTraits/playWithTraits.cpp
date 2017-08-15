29eb8fc04ae8e029c2d0110afc299de8746802e0
Unknown <constant.dupuis@gmail.com>
2017-08-15 18:21:03 +0200

    Add traits sample

--- playWithTraits/playWithTraits.cpp
+++ playWithTraits/playWithTraits.cpp
@@ -2,11 +2,13 @@
 #include <string>
 #include <type_traits>
 
+using namespace std;
+
 template<typename TypeInt>
 class DoSomethingWithInt{
 public:
-    using value_type = typename TypeInt;
-    static_assert(is_integral<TypeInt>(), "Soory, I only take Integral type as parameter.");
+    using value_type = TypeInt;
+    static_assert(is_integral<TypeInt>(), "Sorry, I only take Integral type as parameter.");
 
     DoSomethingWithInt( TypeInt init ): value(init) {}
 
@@ -19,7 +21,8 @@ private:
 int main()
 {
 
-    
+    DoSomethingWithInt<int> dswi1(14);
+    DoSomethingWithInt<float> dswi2(12.135); // should failed at compile time 
 
     return 0;
 }
